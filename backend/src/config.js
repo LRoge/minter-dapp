8bed7b5d6f994caa0282493bd189424a62693cc8
LRoge <rogersliam27@gmail.com>
2022-03-30 00:06:02 +1000

    mint time update

--- backend/src/config.js
+++ backend/src/config.js
@@ -59,10 +59,10 @@ const MINT_PRICE = 0.0001; // Minting price per NFT. Rinkeby = ETH, Polygon = MA
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-30T08:00:48+10:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-03-30T00:10:48+10:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-03-29T00:10:48+10:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 400; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xd77ae73AddB85e7a60A8C424Ce4946c77F1f6d8f"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
